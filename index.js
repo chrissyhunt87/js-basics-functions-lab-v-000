c4193271dd72b1bd5ab19ebcd2b9cb1bfcfa91c7
Chrissy Hunt <chrissy.hunt@gmail.com>
2018-04-06 16:46:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(street) {
+  if (street > 42) {
+    return street - 42;
+  } else {
+    return 42 - street;
+  }
+}
+
+function distanceFromHqInFeet(street) {
+  return distanceFromHqInBlocks(street) * 264;
+}
+
+function distanceTravelledInFeet(start, end) {
+  if (start > end) {
+    return (start - end) * 264;
+  } else {
+    return (end - start) * 264;
+  }
+}
